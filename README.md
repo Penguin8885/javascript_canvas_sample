e1f55e5b2cc3f6a31e4f4db5d418f7c67eed484a
Penguin8885 <penguin.8885@gmail.com>
2019-08-09 15:02:45 +0900

    separate samples

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # javascript_canvas_sample
+
+- random_dotter
 キャンバスにランダムで点を一定間隔で打っていくようなサンプルが書かれています。
 表示結果は以下のような感じです。
 
 <img src="https://github.com/Penguin8885/javascript_canvas_sample/blob/master/sample.gif" alt="サンプル画像" title="サンプル画像">
+
+- canvas_clicker
+キャンバス上でクリックしたところに点を打てます。
---
+++ canvas_clicker/js/main.js
@@ -0,0 +1,80 @@
+﻿function main(){
+    var canvas = document.getElementById('canvas1');
+    if(!canvas || !canvas.getContext){
+        console.log('error : can not load canvas');
+        return false;
+    }
+    var context = canvas.getContext('2d');
+
+    /* イベントリスナーの登録 */
+    canvas.addEventListener('click', onClick, false);
+
+    /* マウスクリックを検出したとき動作する関数 */
+    function onClick(e){
+        var rect = e.target.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+        plotDot(x, y, 0, 0);
+    }
+
+    /* 点を打つ関数 */
+    function plotDot(x, y, marker, color){
+        var size = 10;
+
+        /* 色を指定 */
+        if(color == 0) color = 'rgb(0, 255, 255)';
+        else if(color == 1) color = 'rgb(255, 0, 255)';
+        else if(color == 2) color = 'rgb(255, 255, 0)';
+        else if(color == 3) color = 'rgb(0, 255, 0)';
+        else if(color == 4) color = 'rgb(0, 0, 255)';
+        else if(color == 5) color = 'rgb(255, 0, 0)';
+        else{
+            console.log('error : color is not proper in plotDot');
+            return;
+        }
+
+        /* マーカーを指定、描画 */
+        if(marker == 0) drawCrossDot();
+        else if(marker == 1) drawStrokeCircleDot();
+        else if(marker == 2) drawFillCircleDot();
+        else if(marker == 3) drawStrokeSquareDot();
+        else if(marker == 4) drawFillSquareDot();
+        else{
+            console.log("error : marker is not proper in plotDot");
+            return;
+        }
+
+        function drawCrossDot(){
+            context.beginPath();
+            context.strokeStyle = color;
+            context.moveTo(x-size/2, y-size/2);
+            context.lineTo(x+size/2, y+size/2);
+            context.stroke();
+            context.moveTo(x-size/2, y+size/2);
+            context.lineTo(x+size/2, y-size/2);
+            context.stroke();
+        }
+        function drawStrokeCircleDot(){
+            context.beginPath();
+            context.strokeStyle = color;
+            context.arc(x, y, size/2, 0, Math.PI*2, false);
+            context.stroke();
+        }
+        function drawFillCircleDot(){
+            context.beginPath();
+            context.fillStyle = color;
+            context.arc(x, y, size/2, 0, Math.PI*2, false);
+            context.fill();
+        }
+        function drawStrokeSquareDot(){
+            context.beginPath();
+            context.strokeStyle = color;
+            context.strokeRect(x-size/2, y-size/2, size, size);
+        }
+        function drawFillSquareDot(){
+            context.beginPath();
+            context.fillStyle = color;
+            context.fillRect(x-size/2, y-size/2, size, size);
+        }
+    }
+}
